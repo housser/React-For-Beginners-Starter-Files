308ab84f37f0d1e008b0eced6645c3d55ae2e418
John Housser <John.Housser@arcteryx.com>
2017-10-26 15:50:00 -0700

    Module 11 - event listeners

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,13 +2,29 @@ import React from 'react';
 import { getFunName } from "../helpers"
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  goToStore( event ) {
+    event.preventDefault();
+    // grab the text from the input
+    console.log(this.storeInput.value);
+
+    // transition from / to /store/:storeId 🏬
+  }
+
   render() {
     return (
-      <form action="" className="store-selector">
-        <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
-        <button type="submit">Visit Store ➡</button>
-      </form>
+      <div>
+        {/*<form action="" className="store-selector" onSubmit={this.goToStore.bind(this)}></form>*/}
+        <form action="" className="store-selector" onSubmit={(event) => this.goToStore(event)}>
+          <h2>Please Enter a Store</h2>
+          <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
+          <button type="submit">Visit Store ➡</button>
+        </form>
+      </div>
     );
   }
 }
