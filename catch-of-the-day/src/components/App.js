75bb4b1bb27fab63bbf8151500ed80ab44645252
John Housser <John.Housser@arcteryx.com>
2017-10-27 14:13:37 -0700

    Module 14 - loading data into state onClick

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,8 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
+import sampleFishes from '../sample-fishes';
+
 class App extends React.Component {
   constructor() {
     super();
@@ -15,6 +17,7 @@ class App extends React.Component {
     };
 
     this.addFish = this.addFish.bind( this );
+    this.loadSamples = this.loadSamples.bind( this );
   }
 
   addFish( fish ) {
@@ -28,6 +31,14 @@ class App extends React.Component {
     this.setState( { fishes } );
   }
 
+  loadSamples() {
+    console.log( 'Load 🐠 🐟 🐡' );
+
+    this.setState( {
+      fishes: sampleFishes
+    } )
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -35,7 +46,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order/>
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     );
   }
